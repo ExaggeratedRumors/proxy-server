000617f3294ec2d5d819d4ab7b8e16f4eefb3685
exaggeratedrumors <exaggeratedrumors@gmail.com>
2024-07-28 21:01:19 +0200

    init

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    /** Configuration **/
+    implementation("com.typesafe:config:1.4.2")
+
+    /** Test **/
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(20)
+}
---
+++ src/main/kotlin/utils/Configuration.kt
@@ -0,0 +1,38 @@
+package org.example.utils
+
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
+
+object Configuration {
+    var serverId: Int = 0
+        private set
+    var listenPort: Int = 8080
+        private set
+    var timeout: Int = 20
+        private set
+    lateinit var listenAddresses: String
+        private set
+    lateinit var allowedIpAdresses: List<String>
+        private set
+
+    fun load() {
+        try {
+            val config: Config = ConfigFactory.load("application.conf")
+            val serverConfig = config.getConfig("server")
+            serverId = serverConfig.getInt("server_id")
+            listenPort = serverConfig.getInt("listen_port")
+            timeout = serverConfig.getInt("time_out")
+            listenAddresses = serverConfig.getString("listen_addresses")
+            allowedIpAdresses = serverConfig.getStringList("allowed_ip_addresses")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            error("Configuration: Content of application.conf file is not valid.")
+        }
+    }
+
+    data class CommandEntry(
+        val call: String,
+        val enabled: Boolean,
+        val reqAdmin: Boolean
+    )
+}
