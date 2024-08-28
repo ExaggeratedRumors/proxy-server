0b280add3818d0028b4bfb2da580c5e9acf106b4
exaggeratedrumors <exaggeratedrumors@gmail.com>
2024-08-28 22:03:25 +0200

    create api virtual

--- client/src/main/kotlin/communication/ClientAPI.kt
+++ client/src/main/kotlin/communication/ClientAPI.kt
@@ -1,6 +1,8 @@
 package communication
 
 import dto.Message
+import dto.MessagePayload
+import java.io.File
 
 
 interface ClientAPI {
@@ -9,4 +11,11 @@ interface ClientAPI {
     fun getStatus(): String
     fun getServerStatus(callback: (status: String) -> Unit)
     fun getServerLogs(callback: (message: Message) -> Unit)
+    fun createProducer(topicName: String)
+    fun produce(topicName: String, payload: MessagePayload)
+    fun sendFile(topicName: String, file: File)
+    fun withdrawProducer(topicName: String)
+    fun createSubscriber(topicName: String, callback: (message: Message) -> Unit)
+    fun withdrawSubscriber(topicName: String)
+    fun stop()
 }
